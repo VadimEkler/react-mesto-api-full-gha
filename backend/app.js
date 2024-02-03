e2041642f876deaad2ff0273d4ebae2bfe140e9f
Vadim Ekler <veklercareer@yandex.ru>
2024-02-04 00:19:35 +0700

    bugfix: исправление ошибок автотестов

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URL, {});
+mongoose.connect(DB_URL, {});
 
 app.use(requestLogger);
 
