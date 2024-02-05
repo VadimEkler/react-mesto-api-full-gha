1301c837223788bfcdc62c80fba5ee39ccc1e837
Vadim Ekler <veklercareer@yandex.ru>
2024-02-05 18:13:56 +0700

    добавить краштест

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,20 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use('/signup', signupRoutes);
+
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use('/signin', signinRoutes);
 router.use(auth);
 router.use('/users', userRoutes);
